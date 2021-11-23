55725701c597c7152d82ee4644904bbb48ab40a4
MaxBueno2002 <maxbueno2002@ginebro.cat>
2021-11-23 19:49:58 +0100

    comit despres del lio

--- src/BuenoMax20211123Enu7b.java
+++ src/BuenoMax20211123Enu7b.java
@@ -1,36 +1,35 @@
+
 public class BuenoMax20211123Enu7b {
     public static void main(String[] args) throws Exception {
-        char majuscula=65;   
-        char minsuscula=97;
-        char[] arrayMinus=new char[26];  //Definim l'array de les majuscules i el de les minuscules
-        char[] arrayMajus=new char[26];  //amb 26 posicions ja que hi ha 26 lletres al abecedari
+        char majusculas=65;     //Aqui definim el caracter de majuscula a 65, perque segons el codi ASCII la A majuscula esta a la posicio 65.
+
+        char minsusculas=97;    //Aqui definim el caracter de minuscula a 97 ja que segons el codi ASCII la a minuscula esta en la posicio 97.
+
+
+        char[] arrayMinus=new char[26];  //Definim l'array de les majuscules i el de les minuscules.
+
+        char[] arrayMajus=new char[26];  //amb 26 posicions ja que hi ha 26 lletres al abecedari catalá.
 
         //Aqui s'entren les dades de la a-z i de la A-Z
         for (int i = 0; i < arrayMajus.length; i++) {
-            arrayMajus[i]=majuscula;
-            majuscula++;
-            arrayMinus[i]=minsuscula;
-            minsuscula++;
+            arrayMajus[i]=majusculas;
+            majusculas++;
+            arrayMinus[i]=minsusculas;
+            minsusculas++;
         }
 
-        //Mostro les dades que te l'array de minuscules
+        //Mostro totes les dades en ordre que te l'array de minuscules
         for (int i = 0; i < arrayMinus.length; i++) {
             System.out.print(arrayMinus[i]);
             System.out.print(" ");
         }
 
-        //Mostro les dades que te l'array de majuscules
+        //Mostro totes les dades en ordre que te l'array de majuscules
         for (int i = 0; i < arrayMajus.length; i++) {
             System.out.print(arrayMajus[i]);
             System.out.print(" ");
         }
-
-
-
-
-
-
-
+        
 
 
     }
