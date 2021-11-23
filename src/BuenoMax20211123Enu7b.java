a32068b384d38234c95958a361cb8d34282cb1be
MaxBueno2002 <maxbueno2002@ginebro.cat>
2021-11-23 19:24:29 +0100

    tercer commit

--- src/BuenoMax20211123Enu7b.java
+++ src/BuenoMax20211123Enu7b.java
@@ -1,9 +1,29 @@
 public class BuenoMax20211123Enu7b {
     public static void main(String[] args) throws Exception {
-        char majuscula=65;
+        char majuscula=65;   
         char minsuscula=97;
-        char[] arrMinuscules=new char[26];
-        char[] arrMajuscules=new char[26];
+        char[] arrayMinus=new char[26];  //Definim l'array de les majuscules i el de les minuscules
+        char[] arrayMajus=new char[26];  //amb 26 posicions ja que hi ha 26 lletres al abecedari
+
+        //Aqui s'entren les dades de la a-z i de la A-Z
+        for (int i = 0; i < arrayMajus.length; i++) {
+            arrayMajus[i]=majuscula;
+            majuscula++;
+            arrayMinus[i]=minsuscula;
+            minsuscula++;
+        }
+
+        //Mostro les dades que te l'array de minuscules
+        for (int i = 0; i < arrayMinus.length; i++) {
+            System.out.print(arrayMinus[i]);
+            System.out.print(" ");
+        }
+
+        //Mostro les dades que te l'array de majuscules
+        for (int i = 0; i < arrayMajus.length; i++) {
+            System.out.print(arrayMajus[i]);
+            System.out.print(" ");
+        }
 
 
 
