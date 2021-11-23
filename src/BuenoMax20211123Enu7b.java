acc15d805896702bff464eef8a739e3aeb1ded5e
MaxBueno2002 <maxbueno2002@ginebro.cat>
2021-11-23 19:04:56 +0100

    Segon commit

---
+++ src/BuenoMax20211123Enu7b.java
@@ -0,0 +1,18 @@
+public class BuenoMax20211123Enu7b {
+    public static void main(String[] args) throws Exception {
+        char majuscula=65;
+        char minsuscula=97;
+        char[] arrMinuscules=new char[26];
+        char[] arrMajuscules=new char[26];
+
+
+
+
+
+
+
+
+
+    }
+    
+}
